16cad29760566c006f7a9f26676e236a688b8358
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2022-07-26 18:08:54 -0300

    Add novos botões e estilização

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 new Vue ({
   el: '#app',
   data: {
-    playerLife: 100,
+    running: false,
+    playerLife: 0,
     monsterLife: 100,
   },
 
   computed:{
-
+    hasResult(){
+      return this.playerLife == 0 || this.monsterLife == 0
+    }
   },
 
   methods:{
