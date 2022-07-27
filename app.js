38b130f5060e0837299291237c173335ae1f69d9
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2022-07-26 22:33:29 -0300

    Função de cura implementada e imprimindo logs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue ({
     running: false,
     playerLife: 100,
     monsterLife: 100,
+    log: []
   },
 
   computed:{
@@ -17,17 +18,36 @@ new Vue ({
         this.running = true
         this.playerLife = 100
         this.monsterLife = 100
+        this.log = []
     },
 
     attack(especial){
-      this.hurt('playerLife', 7, 12, false)
-      this.hurt('monsterLife', 5, 10, especial)
+      this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+      if (this.monsterLife > 0){
+        this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+      }
     },
 
-    hurt( prop, min, max, especial){
+    hurt( prop, min, max, especial, source, target, cls){
       const plus = especial ? 5:0
       const hurt = this.getRadom(min + plus, max + plus)
       this[prop] = Math.max (this[prop] - hurt, 0)
+      this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
+    },
+
+    healAndHurt(){
+      this.heal(10, 15)
+      this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+    },
+
+    heal(min, max){
+      const heal = this.getRadom(min, max)
+      this.playerLife = Math.min(this.playerLife + heal, 100)
+      this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
+    },
+
+    registerLog(text, cls){
+      this.log.unshift({text, cls})
     },
 
     getRadom(min, max){
